4d17bc4ef98436039d7f5d7cae5118cde9b73b12
Brandyn Vay <brandynvay@protonmail.com>
2019-10-19 13:59:06 -0700

    base accel that tells you x, y and z axis

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import AccelerometerSensor from "./src/accel.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>aPunchMan</Text>
+      <AccelerometerSensor />
     </View>
   );
 }
@@ -12,9 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
-
